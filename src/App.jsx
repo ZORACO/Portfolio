3880e67f04bc0b8d0cd7f37960b2fe0f994b7f28
ZORACO <amiscisth@gmail.com>
2025-10-06 23:16:48 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+    return (
+        <main>
+            <div>
+                <h1>Welcome to my Portfolio! </h1>
+
+            </div>
+        </main>
+
+    )
+}
+export default App
